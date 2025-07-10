a6a0afdf9280ea50eb3e76a84d5824971ada2b63
sfadfe <seojong2019@gmail.com>
2025-07-10 20:47:23 +0900

    nav bar, footer macro 설정함

--- app.py
+++ app.py
@@ -10,3 +10,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/")
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/about")
+async def about(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request})
